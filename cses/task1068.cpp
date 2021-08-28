6de269a9ec67d9b6011f944e514324e3cf4bf8f5
tiozo <84086606+tiozo@users.noreply.github.com>
2021-08-28 16:44:29 +0700

    Add files via upload

--- cses/task1068.cpp
+++ cses/task1068.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n,k; cin >> n;
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    long long n; cin >> n;
     while (n!=1) {
-        cout << n << ' ';
-        if (n%2==0) n/=2;
-        else n=n*3+1;
-    } cout << 1;
-    return 0;
+        cout << n << " ";
+        if (n%2==1) n=n*3+1;
+        else n/=2;
+    } cout << 1 << " ";
 }

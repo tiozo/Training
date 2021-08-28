45120eb1d42ae753fd94b5972dab94d4e413e3cf
tiozo <84086606+tiozo@users.noreply.github.com>
2021-08-28 16:34:29 +0700

    Add files via upload

--- cses/task1068.cpp
+++ cses/task1068.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     int n,k; cin >> n;
     while (n!=1) {
+        cout << n << ' ';
         if (n%2==0) n/=2;
         else n=n*3+1;
-        cout << n << ' ';
-    }
+    } cout << 1;
     return 0;
 }
